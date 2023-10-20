0b43e86eaddaefb29a83b908e538743f44159aa9
gaetankaler <gaga84120@gmail.com>
2023-10-20 10:00:45 +0200

    ajout du cryptage des mots de passe avec la function SHA2()

--- cinema-BDD/cinemas_ddl.sql
+++ cinema-BDD/cinemas_ddl.sql
@@ -63,7 +63,7 @@ CREATE TABLE `utilisateurs` (
   `nom` varchar(45) NOT NULL,
   `prenom` varchar(45) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `mot_de_passe` varchar(60) NOT NULL,
+  `mot_de_passe` varchar(100) NOT NULL,
   `date_naissance` date NOT NULL,
   `id_role` int NOT NULL,
   PRIMARY KEY (`id_utilisateur`),
